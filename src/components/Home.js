2a5311049ac420d80009b77579168d787820df34
Taylor Jameison <taylorjameison@Taylors-MBP-2.fios-router.home>
2021-12-24 18:35:46 -0500

    adding code for portolio components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,18 @@
 import React from "react";
+import image from "../web-background.jpg";
 
 export default function Home() {
-    return <h1>Home Page!</h1>
+  return (
+    <main>
+      <img
+        src={image}
+        alt="Background Image of Leaves"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex flex-col justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Greetings. I'm Taylor.</h1>
+        <h2 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug">Front End Developer</h2>
+      </section>
+    </main>
+  );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 export default function NavBar() {
   return (
-    <header className="bg-red-600">
+    <header className="bg-green-900">
       <div className="container mx-auto flex justify-between">
         <nav className="flex">
           <NavLink
@@ -11,7 +12,7 @@ export default function NavBar() {
             exact
             className={({ isActive }) =>
               isActive
-                ? "inline-flex items-center py-6 px-3 mr-4 text-whitetext-4xl font-bold cursive tracking-widest"
+                ? "inline-flex items-center py-6 px-3 mr-4 text-white text-4xl font-bold cursive tracking-widest"
                 : "inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
             }
           >
@@ -51,6 +52,11 @@ export default function NavBar() {
             About Me!
           </NavLink>
         </nav>
+        <div className="inline-flex py-3 px-3 my-6">
+            <SocialIcon url="https://twitter.com" className="" target="_blank" fgColor="white" style={{height:35, width:35}}/>
+            <SocialIcon url="https://linkedin.com" className="" target="_blank" fgColor="white" style={{height:35, width:35}}/>
+            <SocialIcon url="https://instagram.com" className="" target="_blank" fgColor="white" style={{height:35, width:35}}/>
+        </div>
       </div>
     </header>
   );
